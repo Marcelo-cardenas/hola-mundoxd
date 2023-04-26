009a5f8d2964cac22913fa59474001101d236038
marce <adrielmarcelo5584gmail.com>
2023-04-26 16:50:33 -0500

    marcelo cardenas

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HOLATIERRA',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
